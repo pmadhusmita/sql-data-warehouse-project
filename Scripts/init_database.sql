313c5f8bc2bbee286205e318247bb55ffd22cdd8
Madhusmita Panda <71130408+pmadhusmita@users.noreply.github.com>
2026-09-14 15:40:12 +0530

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,14 @@
+/*
+==================================================================================
+Create Database and schemas
+==============================================
+Script Purpose:
+     This script creates a new database named 'DataWareHouse" after checking if it already exists.If the database exists,it is dropped and recreated.Additionally,
+     the script sets up three schemas within the database:"bronze","silver", and "gold".
+
+WARNING:
+     Running this script will drop the entire 'DataWareHouse' database if it exists.
+
+
+
+*/
